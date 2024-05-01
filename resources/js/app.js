de9d8d7e3b3a632d2f2a675cae85619f426afcac
ima-yusuke <yusuke1125aadxyz@gmail.com>
2024-05-01 20:30:49 +0900

    update:app.js livewire added 6

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,7 @@
 import './bootstrap';
 import "flowbite";
-import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.esm';
-import Clipboard from '@ryangjchandler/alpine-clipboard'
+import Alpine from 'alpinejs';
 
-Alpine.plugin(Clipboard)
+window.Alpine = Alpine;
 
-Livewire.start()
-
-// window.Alpine = Alpine;
-
-// Alpine.start();
+Alpine.start();
