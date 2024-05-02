27670d8ce775d8fd776fdeff5be99bafa98e773a
ima-yusuke <yusuke1125aadxyz@gmail.com>
2024-05-02 16:41:34 +0900

    update:layout 追加

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{$title}}</title>
 
-{{--    @livewireStyles--}}
+    @livewireStyles
     @vite('resources/css/app.css')
     @vite('resources/js/index.js')
 
@@ -15,7 +15,7 @@
 </head>
 <body class="overflow-hidden">
 {{$slot}}
-{{--@livewireScriptConfig--}}
+@livewireScriptConfig
 </body>
 
 </html>
