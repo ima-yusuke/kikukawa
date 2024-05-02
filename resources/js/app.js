1481065a185c9a4a2a8919b961bebe47ca16135d
ima-yusuke <yusuke1125aadxyz@gmail.com>
2024-05-02 16:34:43 +0900

    update:app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,9 @@
 import './bootstrap';
 import "flowbite";
 import Alpine from 'alpinejs';
+import { Livewire } from '../../vendor/livewire/livewire/dist/livewire.esm'
+
+Livewire.start()
 
 window.Alpine = Alpine;
 
