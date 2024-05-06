52997a51e28cc58320e8550e31f33c452c6361fe
ima-yusuke <yusuke1125aadxyz@gmail.com>
2024-05-06 11:23:23 +0900

    update:管理者用（自分）アカウント追加

---
+++ database/migrations/2024_05_06_112138_add_admin_user.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        User::create([
+            'name'=>'yusuke',
+            'email'=>'yusuke@com',
+            'password'=>Hash::make('password'),
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
